85e922960bc44ae0fee08e1be16ebb1c240da923
empipio <94937507+empipio@users.noreply.github.com>
2022-03-14 21:48:15 +0000

    files created, schema done

---
+++ server.js
@@ -0,0 +1,23 @@
+const inquirer = require("inquirer");
+const express = require("express");
+const mysql = require("mysql2");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "P0ta55ium",
+    database: "company_db",
+  },
+  console.log(`Connected to the company_db database.`)
+);
