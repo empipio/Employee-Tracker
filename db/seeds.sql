35426b67cdc1d920b5cd4722c038560827610faa
empipio <94937507+empipio@users.noreply.github.com>
2022-03-16 15:26:07 +0000

    seeds added, start to sketch out plan

---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+INSERT INTO department (name)
+VALUES ("Accounts and Finance"),
+("Sales and Marketing"),
+("Product Development"),
+("HR"),
+("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accounts Manager", 50000, 1), 
+("Accountant", 40000, 1), 
+("Sales Executive", 60000, 2), 
+("Sales Intern", 25000, 2),
+("Lead Engineer", 80000, 3),
+("Software Engineer", 60000, 3),
+("Software Intern", 35000, 3),
+("HR Manager", 50000, 4),
+("Legal Lead", 70000, 5),
+("Lawyer", 55000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Bob", "Robinson", 1, null),
+("Carole", "Roberts", 2, 1),
+("Max", "Power", 3, null),
+("Homer", "Simpson", 4, 2),
+("Harold", "Window", 5, null),
+("Frodo", "Baggins", 6, 3),
+("Samwise", "Gamgee", 7, 3),
+("Sally", "Sampson", 8, null),
+("Elle", "Woods", 9, null),
+("Barry", "Chuckle", 10, 4);
--- server.js
+++ server.js
@@ -1,6 +1,34 @@
+//NEED TO DO SEEDS
+
+// 1. INQUIRER PROMPTS
+// CHECKBOX-what would you like to do?
+//view all departments
+//view all roles
+//view all employees
+//add a department
+//add a role
+//add an employee
+//update an employee role
+
+//VIEW ALL DEPARTMENTS -> table with department names and IDs (use console.table?)
+
+//VIEW ALL ROLES -> table with job title, role id, the department that role belongs to, and the salary for that role
+
+//VIEW ALL EMPLOYEES -> employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+//ADD A DEPARTMENT -> enter department name, and it is added to database
+
+//ADD A ROLE -> enter the name, salary, and department for the role and that role is added to the database
+
+//ADD AN EMPLOYEE -> enter the first name, last name, role, and manager, and that employee is added to the database
+
+//UPDATE EMPLOYEE ROLE -> select an employee to update and their new role and this information is updated in the database
+// pick name and role from list?
+
 const inquirer = require("inquirer");
 const express = require("express");
 const mysql = require("mysql2");
+const consoleTable = require("console.table");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -21,3 +49,18 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the company_db database.`)
 );
+
+// Bonus
+// Fulfilling any of the following can add up to 20 points to your grade.
+//Note that the highest grade you can achieve is still 100:
+
+// Application allows users to update employee managers (2 points).
+
+// Application allows users to view employees by manager (2 points).
+
+// Application allows users to view employees by department (2 points).
+
+// Application allows users to delete departments, roles, and employees (2 points for each).
+
+// Application allows users to view the total utilized budget of a department—in other words, the combined salaries of all
+//employees in that department (8 points).
